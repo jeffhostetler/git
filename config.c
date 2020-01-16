99dbd573e411814c6c42c2ab0d2a57be70a365a0
Jeff Hostetler <jeffhost@microsoft.com>
2020-01-16 10:42:04 -0500

    config: add core.parallelCheckout config setting
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- config.c
+++ config.c
@@ -1404,6 +1404,11 @@ static int git_default_core_config(const char *var, const char *value, void *cb)
 		return 0;
 	}
 
+	if (!strcmp(var, "core.parallelcheckout")) {
+		core_parallel_checkout = git_config_bool(var, value);
+		return 0;
+	}
+
 	/* Add other config variables here and to Documentation/config.txt. */
 	return platform_core_config(var, value, cb);
 }
