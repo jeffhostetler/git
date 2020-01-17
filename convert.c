7f0b61997afdf904e388bd38c69fc13de8957046
Jeff Hostetler <jeffhost@microsoft.com>
2020-01-17 10:25:21 -0500

    convert: add convert_to_working_tree_ca()
    Create new public interface to `convert_to_working_tree()` that takes
    a precomputed `struct conv_attrs` and does not rely on the index.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- convert.c
+++ convert.c
@@ -1523,6 +1523,14 @@ int convert_to_working_tree(const struct index_state *istate,
 	return convert_to_working_tree_internal(istate, path, src, len, dst, 0, meta, NULL);
 }
 
+int convert_to_working_tree_ca(const struct conv_attrs *ca,
+			       const char *path, const char *src,
+			       size_t len, struct strbuf *dst,
+			       const struct checkout_metadata *meta)
+{
+	return convert_to_working_tree_internal_ca(ca, path, src, len, dst, 0, meta, NULL);
+}
+
 int renormalize_buffer(const struct index_state *istate, const char *path,
 		       const char *src, size_t len, struct strbuf *dst)
 {
