52e2f3eeba8dc8d9904e468a129b725f171d3ec6
Jeff Hostetler <jeffhost@microsoft.com>
2020-05-04 10:24:47 -0400

    unpack-trees: add Trace2 regions
    Add regions around `update_sparsity()`, `check_updates()` and cache-tree
    operations in `unpack-trees.c`
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- cache-tree.c
+++ cache-tree.c
@@ -6,6 +6,7 @@
 #include "object-store.h"
 #include "replace-object.h"
 #include "promisor-remote.h"
+#include "trace2.h"
 
 #ifndef DEBUG_CACHE_TREE
 #define DEBUG_CACHE_TREE 0
@@ -733,10 +734,14 @@ void prime_cache_tree(struct repository *r,
 		      struct index_state *istate,
 		      struct tree *tree)
 {
+	trace2_region_enter("cache_tree", "prime", NULL);
+
 	cache_tree_free(&istate->cache_tree);
 	istate->cache_tree = cache_tree();
 	prime_cache_tree_rec(r, istate->cache_tree, tree);
 	istate->cache_changed |= CACHE_TREE_CHANGED;
+
+	trace2_region_leave("cache_tree", "prime", NULL);
 }
 
 /*
