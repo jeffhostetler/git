041b32d0a58bb6b8ce6a1e4f1adf84b82a13da31
Jeff Hostetler <jeffhost@microsoft.com>
2020-05-22 15:05:36 -0400

    fixup do_proto

--- builtin/parallel-checkout-helper.c
+++ builtin/parallel-checkout-helper.c
@@ -3,6 +3,15 @@
 #include "config.h"
 #include "parse-options.h"
 #include "pkt-line.h"
+#include "trace2.h"
+
+/*
+ * This is used as a label prefix in all error messages
+ * and in any trace2 messages to identify this child
+ * process.
+ */
+static char t2_child_name[100];
+static int t2_child_nr = -1;
 
 static const char * const helper_usage[] = {
 	N_("git parallel-checkout-helper [<options>]"),
@@ -38,8 +47,14 @@ static int do_protocol_handshake(void)
 	int b_support_our_version = 0;
 
 	len = packet_read_line_gently(0, NULL, &line);
-	if (len < 0 || !line || strcmp(line, "parallel-checkout-helper-client")) {
-		error("server: subprocess welcome handshake failed: %s", line);
+	if (len < 0 || !line) {
+		error("%s: subprocess welcome handshake failed",
+		      t2_child_name);
+		return 1;
+	}
+	if (strcmp(line, "parallel-checkout-helper-client")) {
+		error("%s: subprocess welcome handshake failed: %s",
+		      t2_child_name, line);
 		return 1;
 	}
 
@@ -49,15 +64,15 @@ static int do_protocol_handshake(void)
 		if (len < 0 || !line)
 			break;
 		if (!skip_prefix(line, "version=", &v)) {
-			error("server: subprocess version handshake failed: %s",
-			      line);
+			error("%s: subprocess version handshake failed: %s",
+			      t2_child_name, line);
 			return 1;
 		}
 		b_support_our_version |= (!strcmp(v, OUR_SUBPROCESS_VERSION));
 	}
 	if (!b_support_our_version) {
-		error("server: client does not support our version: %s",
-		      OUR_SUBPROCESS_VERSION);
+		error("%s: client does not support our version: %s",
+		      t2_child_name, OUR_SUBPROCESS_VERSION);
 		return 1;
 	}
 
@@ -65,7 +80,7 @@ static int do_protocol_handshake(void)
 	    packet_write_fmt_gently(1, "version=%s\n",
 				    OUR_SUBPROCESS_VERSION) ||
 	    packet_flush_gently(1)) {
-		error("server: cannot write version handshake");
+		error("%s: cannot write version handshake", t2_child_name);
 		return 1;
 	}
 
@@ -77,8 +92,8 @@ static int do_protocol_handshake(void)
 		if (len < 0 || !line)
 			break;
 		if (!skip_prefix(line, "capability=", &v)) {
-			error("server: subprocess capability handshake failed: %s",
-			      line);
+			error("%s: subprocess capability handshake failed: %s",
+			      t2_child_name, line);
 			return 1;
 		}
 		for (k = 0; caps[k].name; k++)
@@ -90,12 +105,12 @@ static int do_protocol_handshake(void)
 		if (caps[k].client_has)
 			if (packet_write_fmt_gently(1, "capability=%s\n",
 						    caps[k].name)) {
-				error("server: cannot write capabilities handshake: %s",
-				      caps[k].name);
+				error("%s: cannot write capabilities handshake: %s",
+				      t2_child_name, caps[k].name);
 				return 1;
 			}
 	if (packet_flush_gently(1)) {
-		error("server: cannot write capabilities handshake");
+		error("%s: cannot write capabilities handshake", t2_child_name);
 		return 1;
 	}
 
@@ -108,6 +123,7 @@ int cmd_parallel_checkout_helper(int argc, const char **argv,
 	int err = 0;
 
 	struct option helper_options[] = {
+		OPT_INTEGER(0, "child", &t2_child_nr, N_("child number")),
 		OPT_END()
 	};
 
@@ -117,6 +133,10 @@ int cmd_parallel_checkout_helper(int argc, const char **argv,
 	git_config(git_default_config, NULL);
 	argc = parse_options(argc, argv, prefix, helper_options,
 			     helper_usage, 0);
+
+	snprintf(t2_child_name, sizeof(t2_child_name),
+		 "helper[%02d]", t2_child_nr);
+	packet_trace_identity(t2_child_name);
 
 	if (do_protocol_handshake())
 		return 1;
