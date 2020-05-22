5462e4bdd3c1575bba8bda693da3aeb18f4da4d9
Jeff Hostetler <jeffhost@microsoft.com>
2020-05-22 13:32:00 -0400

    pch: add parse-option and config

--- builtin/parallel-checkout-helper.c
+++ builtin/parallel-checkout-helper.c
@@ -1,10 +1,28 @@
 #include "builtin.h"
 #include "cache.h"
+#include "config.h"
+#include "parse-options.h"
+
+static const char * const helper_usage[] = {
+	N_("git parallel-checkout-helper [<options>]"),
+	NULL
+};
 
 int cmd_parallel_checkout_helper(int argc, const char **argv,
 				 const char *prefix)
 {
 	int err = 0;
+
+	struct option helper_options[] = {
+		OPT_END()
+	};
+
+	if (argc == 2 && !strcmp(argv[1], "-h"))
+		usage_with_options(helper_usage, helper_options);
+
+	git_config(git_default_config, NULL);
+	argc = parse_options(argc, argv, prefix, helper_options,
+			     helper_usage, 0);
 
 	return err;
 }
