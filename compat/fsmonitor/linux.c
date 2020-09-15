bbd7ec7bcb81b5e14de8fe61994bbf43509fcfe8
Jeff Hostetler <jeffhost@microsoft.com>
2020-09-15 14:35:29 -0400

    WIP fsmonitor linux API change

--- compat/fsmonitor/linux.c
+++ compat/fsmonitor/linux.c
@@ -8,8 +8,8 @@ KHASH_INIT(path2wd, const char *, int, 1, kh_str_hash_func, kh_str_hash_equal);
 #define kh_int_hash_equal(a, b) ((a) == (b))
 KHASH_INIT(wd2path, int, const char *, 1, kh_int_hash_func, kh_int_hash_equal);
 
-struct backend_data {
-	int fd;
+struct fsmonitor_daemon_backend_data {
+	int fd_inotify;
 	kh_path2wd_t *path2wd;
 	kh_wd2path_t *wd2path;
 };
@@ -20,8 +20,8 @@ static int watch_directory(struct fsmonitor_daemon_state *state,
 	uint32_t mask = IN_CLOSE_WRITE | IN_CREATE |
 		IN_DELETE | IN_DELETE_SELF | IN_MODIFY |
 		IN_MOVE_SELF | IN_MOVED_FROM | IN_MOVED_TO;
-	struct backend_data *data = state->backend_data;
-	int wd = inotify_add_watch(data->fd, *path ? path : ".", mask);
+	struct fsmonitor_daemon_backend_data *data = state->backend_data;
+	int wd = inotify_add_watch(data->fd_inotify, *path ? path : ".", mask);
 	int ret1, ret2;
 	khint_t pos1, pos2;
 
@@ -43,7 +43,7 @@ static int watch_directory(struct fsmonitor_daemon_state *state,
 			kh_del_path2wd(data->path2wd, pos1);
 		if (pos2)
 			kh_del_wd2path(data->wd2path, pos2);
-		inotify_rm_watch(data->fd, wd);
+		inotify_rm_watch(data->fd_inotify, wd);
 		return error(_("could not watch '%s"), path);
 	}
 
@@ -98,7 +98,7 @@ static int watch_directory_recursively(struct fsmonitor_daemon_state *state,
 static int unwatch_directory(struct fsmonitor_daemon_state *state,
 			     const char *path)
 {
-	struct backend_data *data = state->backend_data;
+	struct fsmonitor_daemon_backend_data *data = state->backend_data;
 	khint_t pos = kh_get_path2wd(data->path2wd, path);
 
 	if (pos < kh_end(data->path2wd)) {
@@ -109,7 +109,7 @@ static int unwatch_directory(struct fsmonitor_daemon_state *state,
 		kh_del_path2wd(data->path2wd, pos);
 		if (pos2 < kh_end(data->wd2path))
 			kh_del_wd2path(data->wd2path, pos2);
-		return inotify_rm_watch(data->fd, wd);
+		return inotify_rm_watch(data->fd_inotify, wd);
 	}
 
 	return 0; /* ignore unseen path */
@@ -126,13 +126,13 @@ struct fsmonitor_daemon_state *fsmonitor_listen(
 	uint32_t dir_created = IN_CREATE | IN_ISDIR;
 	uint32_t dir_deleted = IN_DELETE | IN_ISDIR;
 	struct strbuf buf = STRBUF_INIT;
-	struct backend_data data;
+	struct fsmonitor_daemon_backend_data data;
 	int ret;
 
 	trace2_region_enter("fsmonitor", "inotify", the_repository);
 
-	data.fd = inotify_init();
-	if (data.fd < 0) {
+	data.fd_inotify = inotify_init();
+	if (data.fd_inotify < 0) {
 		error_errno(_("could not initialize inotify"));
 		trace2_region_leave("fsmonitor", "inotify", the_repository);
 		return state;
@@ -152,21 +152,21 @@ struct fsmonitor_daemon_state *fsmonitor_listen(
 
 	trace2_printf("Start watching: '%s' for inotify", get_git_work_tree());
 
-	while (data.fd >= 0) {
+	while (data.fd_inotify >= 0) {
 		struct fsmonitor_queue_item dummy, *queue = &dummy;
 		uint64_t time = getnanotime();
 		char b[sizeof(struct inotify_event) + NAME_MAX + 1], *p;
-		int ret = read(data.fd, &b, sizeof(b)), i;
+		int ret = read(data.fd_inotify, &b, sizeof(b)), i;
 
 		if (ret < 0) {
 
 			// TODO Guard this (and perhaps the above read() call
-			// TODO from the case where (data.fd == -1) because
+			// TODO from the case where (data.fd_inotify == -1) because
 			// TODO another thread called fsmonitor_listen_stop()
 			// TODO (with or without any locking) and closed the
 			// TODO fd.
 			//
-			// TODO investigate the locking around access to data.fd.
+			// TODO investigate the locking around access to data.fd_inotify.
 			//
 			error_errno(_("could not read() inotify fd"));
 			goto out;
@@ -259,15 +259,15 @@ out:
 
 		kh_foreach(data.path2wd, path, wd, {
 			free((char *)path);
-			if (data.fd >= 0)
-				inotify_rm_watch(data.fd, wd);
+			if (data.fd_inotify >= 0)
+				inotify_rm_watch(data.fd_inotify, wd);
 		});
 	}
 	kh_release_path2wd(data.path2wd);
 	kh_release_wd2path(data.wd2path);
-	if (data.fd >= 0) {
-		close(data.fd);
-		data.fd = -1;
+	if (data.fd_inotify >= 0) {
+		close(data.fd_inotify);
+		data.fd_inotify = -1;
 	}
 	strbuf_release(&buf);
 	fsmonitor_listen_stop(state);
@@ -277,11 +277,11 @@ out:
 
 int fsmonitor_listen_stop(struct fsmonitor_daemon_state *state)
 {
-	struct backend_data *data = state->backend_data;
+	struct fsmonitor_daemon_backend_data *data = state->backend_data;
 
 	error(_("fsmonitor was told to stop"));
-	close(data->fd);
-	data->fd = -1;
+	close(data->fd_inotify);
+	data->fd_inotify = -1;
 
 	return 0;
 }
