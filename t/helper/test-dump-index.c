b7e6c08e807924aac4c7147b4f59d8ee591a5911
Jeff Hostetler <jeffhost@microsoft.com>
2022-04-06 10:43:50 -0400

    t/helper/test-dump-index: compare cache mtime with observed mtime
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- t/helper/test-dump-index.c
+++ t/helper/test-dump-index.c
@@ -5,6 +5,44 @@
 #include "tree.h"
 #include "commit.h"
 
+static void print_lstat_delta_entry(struct cache_entry *ce)
+{
+	const struct stat_data *sd = &ce->ce_stat_data;
+	struct stat st;
+	struct stat_data sd_now;
+
+	if (lstat(ce->name, &st) < 0)
+		die_errno("could not lstat('%s')", ce->name);
+	fill_stat_data(&sd_now, &st);
+
+	if ((sd_now.sd_ctime.sec > sd->sd_ctime.sec) ||
+	    (sd_now.sd_ctime.sec == sd->sd_ctime.sec && sd_now.sd_ctime.nsec > sd->sd_ctime.nsec))
+		printf(" [d_ctime: +]");
+	else if (sd_now.sd_ctime.sec == sd->sd_ctime.sec && sd_now.sd_ctime.nsec == sd->sd_ctime.nsec)
+		printf(" [d_ctime: =]");
+	else
+		printf(" [d_ctime: -]");
+
+	if ((sd_now.sd_mtime.sec > sd->sd_mtime.sec) ||
+	    (sd_now.sd_mtime.sec == sd->sd_mtime.sec && sd_now.sd_mtime.nsec > sd->sd_mtime.nsec))
+		printf(" [d_mtime: +]");
+	else if (sd_now.sd_mtime.sec == sd->sd_mtime.sec && sd_now.sd_mtime.nsec == sd->sd_mtime.nsec)
+		printf(" [d_mtime: =]");
+	else
+		printf(" [d_mtime: -]");
+
+	printf(" [ce_flags: %08x]", ce->ce_flags);
+
+	printf("\t%s\n", ce->name);
+}
+
+static void print_lstat_delta(struct index_state *istate)
+{
+	int k;
+	for (k = 0; k < istate->cache_nr; k++)
+		print_lstat_delta_entry(istate->cache[k]);
+}
+
 static void print_cache_entry(struct cache_entry *ce)
 {
 	const char *type;
@@ -50,9 +88,15 @@ int cmd__dump_index(int argc, const char **argv)
 	printf("===Initial===\n");
 	print_cache(r->index);
 
+	printf("===Delta===\n");
+	print_lstat_delta(r->index);
+
 	refresh_index(r->index, REFRESH_QUIET, NULL, NULL, NULL);
 	printf("===Refreshed===\n");
 	print_cache(r->index);
+
+	printf("===Delta After Refresh===\n");
+	print_lstat_delta(r->index);
 
 	discard_index(r->index);
 
