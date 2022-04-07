4bd71c339152ecba1d0bb8e4f2c188f209402941
Jeff Hostetler <jeffhost@microsoft.com>
2022-04-07 11:21:12 -0400

    t/helper/dump-index: add rollback to backdate index mtimes
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- t/helper/test-dump-index.c
+++ t/helper/test-dump-index.c
@@ -4,6 +4,34 @@
 #include "blob.h"
 #include "tree.h"
 #include "commit.h"
+#include "parse-options.h"
+
+static const char * const dump_index_usage[] = {
+	"test-tool dump-index [<options>]",
+	"",
+	"Dumps the contents of the index.",
+	NULL
+};
+
+static int print_initial_index = 1;
+static int print_initial_delta = 0;
+static int do_rollback = 0;
+static int print_rollback_delta = 0;
+static int print_refresh_index = 0;
+static int print_refresh_delta = 0;
+
+static struct option options[] = {
+	OPT_BOOL(0, "print-initial-index", &print_initial_index, "print initial index as read from disk"),
+	OPT_BOOL(0, "print-initial-delta", &print_initial_delta, "print wd delta from initial index"),
+
+	OPT_BOOL(0, "rollback", &do_rollback, "backdate index mtimes before refresh"),
+	OPT_BOOL(0, "print-rollback-delta", &print_rollback_delta, "print wd delta after rollback"),
+
+	OPT_BOOL(0, "print-refresh-index", &print_refresh_index, "print refreshed index"),
+	OPT_BOOL(0, "print-refresh-delta", &print_refresh_delta, "print wd delta from refreshed index"),
+	OPT_END()
+};
+
 
 static void print_lstat_delta_entry(struct cache_entry *ce)
 {
@@ -75,9 +103,28 @@ static void print_cache(struct index_state *istate)
 		print_cache_entry(istate->cache[k]);
 }
 
+static void rollback_index_entry(struct cache_entry *ce)
+{
+	struct stat_data *sd = &ce->ce_stat_data;
+
+	sd->sd_mtime.sec -= 5;
+}
+
+static void rollback_index(struct index_state *istate)
+{
+	int k;
+	for (k = 0; k < istate->cache_nr; k++)
+		rollback_index_entry(istate->cache[k]);
+}
+
+
 int cmd__dump_index(int argc, const char **argv)
 {
 	struct repository *r = the_repository;
+
+	argc = parse_options(argc, argv, NULL, options, dump_index_usage, 0);
+	if (argc)
+		usage_with_options(dump_index_usage, options);
 
 	initialize_the_repository();
 
@@ -85,18 +132,36 @@ int cmd__dump_index(int argc, const char **argv)
 	git_config(git_default_config, NULL);
 
 	repo_read_index(r);
-	printf("===Initial===\n");
-	print_cache(r->index);
 
-	printf("===Delta===\n");
-	print_lstat_delta(r->index);
+	if (print_initial_index) {
+		printf("===Initial===\n");
+		print_cache(r->index);
+	}
+
+	if (print_initial_delta) {
+		printf("===Delta===\n");
+		print_lstat_delta(r->index);
+	}
+
+	if (do_rollback) {
+		rollback_index(r->index);
+		if (print_rollback_delta) {
+			printf("===Delta After Rollback===\n");
+			print_lstat_delta(r->index);
+		}
+	}
 
 	refresh_index(r->index, REFRESH_QUIET, NULL, NULL, NULL);
-	printf("===Refreshed===\n");
-	print_cache(r->index);
 
-	printf("===Delta After Refresh===\n");
-	print_lstat_delta(r->index);
+	if (print_refresh_index) {
+		printf("===Refreshed===\n");
+		print_cache(r->index);
+	}
+
+	if (print_refresh_delta) {
+		printf("===Delta After Refresh===\n");
+		print_lstat_delta(r->index);
+	}
 
 	discard_index(r->index);
 
